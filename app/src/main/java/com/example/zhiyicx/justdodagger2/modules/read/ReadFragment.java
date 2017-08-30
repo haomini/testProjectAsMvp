520dc529fab4a54003ac7a8baa82ee453565f25d
haomini <605626708qq@gmail.com>
2017-08-31 01:42:45 +0800

    添加部分数据

--- app/src/main/java/com/example/zhiyicx/justdodagger2/modules/read/ReadFragment.java
+++ app/src/main/java/com/example/zhiyicx/justdodagger2/modules/read/ReadFragment.java
@@ -61,6 +61,51 @@ public class ReadFragment extends BaseFragment {
         bean2.setPath("http://www.cnblogs.com/keanuyaoo/archive/2013/09/04/3301826.html");
         list.add(bean2);
 
+        ReadBean bean3 = new ReadBean();
+        bean3.setName("更我一起学系列{3}");
+        bean3.setPath("http://www.cnblogs.com/keanuyaoo/archive/2013/09/04/3301826.html");
+        list.add(bean3);
+
+        ReadBean bean4 = new ReadBean();
+        bean4.setName("更我一起学系列{4}");
+        bean4.setPath("http://www.cnblogs.com/keanuyaoo/archive/2013/09/04/3301826.html");
+        list.add(bean4);
+
+        ReadBean bean5 = new ReadBean();
+        bean5.setName("更我一起学系列{5}");
+        bean5.setPath("http://www.cnblogs.com/keanuyaoo/archive/2013/09/04/3301826.html");
+        list.add(bean5);
+
+        ReadBean bean6 = new ReadBean();
+        bean6.setName("更我一起学系列{6}");
+        bean6.setPath("http://www.cnblogs.com/keanuyaoo/archive/2013/09/04/3301826.html");
+        list.add(bean6);
+
+        ReadBean bean7 = new ReadBean();
+        bean7.setName("更我一起学系列{7}");
+        bean7.setPath("http://www.cnblogs.com/keanuyaoo/archive/2013/09/04/3301826.html");
+        list.add(bean7);
+
+        ReadBean bean8 = new ReadBean();
+        bean8.setName("更我一起学系列{8}");
+        bean8.setPath("http://www.cnblogs.com/keanuyaoo/archive/2013/09/04/3301826.html");
+        list.add(bean8);
+
+
+        ReadBean bean9 = new ReadBean();
+        bean9.setName("更我一起学系列{9}");
+        bean9.setPath("http://www.cnblogs.com/keanuyaoo/archive/2013/09/04/3301826.html");
+        list.add(bean9);
+
+        ReadBean bean10 = new ReadBean();
+        bean10.setName("更我一起学系列{10}");
+        bean10.setPath("http://www.cnblogs.com/keanuyaoo/archive/2013/09/04/3301826.html");
+        list.add(bean10);
+
+        ReadBean bean11 = new ReadBean();
+        bean11.setName("更我一起学系列{11}");
+        bean11.setPath("http://www.cnblogs.com/keanuyaoo/archive/2013/09/04/3301826.html");
+        list.add(bean11);
         return list;
     }
 
